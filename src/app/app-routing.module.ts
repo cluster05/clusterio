37307f0891e066714a511d57bdadf4fdda91f3bc
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-04 20:58:30 +0530

    Page Not Found page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -7,8 +8,12 @@ const routes: Routes = [
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
   {
-    path: 'cluster',
+    path: '',
     loadChildren: () => import('./cluster/cluster.module').then(m => m.ClusterModule)
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [ButtonComponent, InputComponent],
+  declarations: [ButtonComponent, InputComponent, PageNotFoundComponent],
   imports: [
     CommonModule
   ]
