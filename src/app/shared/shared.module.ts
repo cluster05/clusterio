36994c377955627654bf5298edfe7d34e3b5165d
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-05 21:14:15 +0530

    added button and tag style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './components/button/button.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { QuillEditorComponent } from './components/quill-editor/quill-editor.component';
 import { FormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill';
 
 const components = [
-  ButtonComponent,
   PageNotFoundComponent,
   QuillEditorComponent
 ];
