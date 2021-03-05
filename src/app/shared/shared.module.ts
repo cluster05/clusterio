6a3e17669bde5b061a43799c1703047608cd2fb6
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-05 12:15:25 +0530

    Quill Editor Added.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,28 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { QuillEditorComponent } from './components/quill-editor/quill-editor.component';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
-
+const components = [
+  ButtonComponent,
+  InputComponent,
+  PageNotFoundComponent,
+  QuillEditorComponent
+];
 
 @NgModule({
-  declarations: [ButtonComponent, InputComponent, PageNotFoundComponent],
+  declarations: [
+    components
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    QuillModule
+  ],
+  exports: [
+    components
   ]
 })
 export class SharedModule { }
