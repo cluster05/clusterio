129e83c83b8fb9f0e8a79339c90ac4e01b278877
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-06 22:15:24 +0530

    added single article,snippet and view post componets.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
 import { SinglePostComponent } from './components/single-post/single-post.component';
+import { SingleSnippetComponent } from './components/single-snippet/single-snippet.component';
+import { SingleArticleComponent } from './components/single-article/single-article.component';
+import { ViewPostComponent } from './components/view-post/view-post.component';
 
 const components = [
   PageNotFoundComponent,
@@ -12,6 +15,9 @@ const components = [
   declarations: [
     components,
     SinglePostComponent,
+    SingleSnippetComponent,
+    SingleArticleComponent,
+    ViewPostComponent,
   ],
   imports: [
     CommonModule,
