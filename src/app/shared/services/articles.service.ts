ee4541c0a79bb2124c87e450eb2d18532187d8a5
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-06 21:04:36 +0530

    added all method for backend call in article.

--- src/app/shared/services/articles.service.ts
+++ src/app/shared/services/articles.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  read(articleId: string) {
+    return this.http.get(environment.backendBaseURL + '/articles/' + articleId);
+  }
+
+  readAllArticle() {
+    return this.http.get(environment.backendBaseURL + '/articles');
+  }
+
+  createArticle(payload: any) {
+    return this.http.post(environment.backendBaseURL + '/articles', payload);
+  }
+
+  updateArticle(articleId: string, payload: any) {
+    return this.http.patch(environment.backendBaseURL + '/articles/' + articleId, payload);
+  }
+
+  deleteArticle(articleId: string) {
+    return this.http.delete(environment.backendBaseURL + '/articles/' + articleId);
+  }
 }
