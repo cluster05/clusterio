a8707bd9ea4b8a09654bea140d8cf2e83e983708
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-06 21:42:58 +0530

    manage subscription in create snippet and article.

--- src/app/admin/components/manage-articles/build-article/build-article.component.ts
+++ src/app/admin/components/manage-articles/build-article/build-article.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Article } from 'src/app/constant/interface/post.interface';
 import { ArticlesService } from 'src/app/shared/services/articles.service';
 
@@ -8,9 +9,11 @@ import { ArticlesService } from 'src/app/shared/services/articles.service';
   templateUrl: './build-article.component.html',
   styleUrls: ['./build-article.component.scss']
 })
-export class BuildArticleComponent implements OnInit {
+export class BuildArticleComponent implements OnInit, OnDestroy {
 
   buildType = 'article';
+
+  subscription: Subscription = new Subscription()
 
   constructor(
     private articleService: ArticlesService,
@@ -22,10 +25,13 @@ export class BuildArticleComponent implements OnInit {
   }
 
   outputPost(article: Article): void {
-    this.articleService.createArticle(article).subscribe(
+    this.subscription = this.articleService.createArticle(article).subscribe(
       response => this.router.navigate(['../',], { relativeTo: this.route }),
       error => alert(error.error.message)
     )
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/admin/components/manage-quick-snippet/build-quick-snippet/build-quick-snippet.component.ts
+++ src/app/admin/components/manage-quick-snippet/build-quick-snippet/build-quick-snippet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Article } from 'src/app/constant/interface/post.interface';
 import { ArticlesService } from 'src/app/shared/services/articles.service';
 @Component({
@@ -10,6 +11,7 @@ import { ArticlesService } from 'src/app/shared/services/articles.service';
 export class BuildQuickSnippetComponent implements OnInit {
 
   buildType = 'snippet';
+  subscription: Subscription = new Subscription()
 
   constructor(
     private articleService: ArticlesService,
@@ -21,10 +23,13 @@ export class BuildQuickSnippetComponent implements OnInit {
   }
 
   outputPost(article: Article): void {
-    this.articleService.createArticle(article).subscribe(
+    this.subscription = this.articleService.createArticle(article).subscribe(
       response => this.router.navigate(['../',], { relativeTo: this.route }),
       error => alert(error.error.message)
     )
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
