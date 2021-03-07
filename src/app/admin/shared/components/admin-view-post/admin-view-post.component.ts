5539c65277326f602860850c05816e81393ef829
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-07 19:24:00 +0530

    call API for single post retriew.

--- src/app/admin/shared/components/admin-view-post/admin-view-post.component.ts
+++ src/app/admin/shared/components/admin-view-post/admin-view-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Article } from 'src/app/constant/interface/post.interface';
+import { ArticlesService } from 'src/app/shared/services/articles.service';
 
 @Component({
   selector: 'cluster-admin-view-post',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminViewPostComponent implements OnInit {
 
-  constructor() { }
+
+  post: Article = {
+    type: 'article',
+    title: 'article title is loading ...',
+    description: '',
+    tags: [],
+    content: '',
+  };
+  subscription: Subscription = new Subscription();
+
+  constructor(private articleService: ArticlesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    const postId = this.route.snapshot.paramMap.get('postId') as string
+
+    this.subscription = this.articleService.readArticle(postId)
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => alert(error.error.message)
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
