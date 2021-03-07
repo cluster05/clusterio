8f91cf9a8757091af258dd37d92b2f751c76cc6d
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-07 18:02:48 +0530

    remove unused componets.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
-import { SinglePostComponent } from './components/single-post/single-post.component';
 import { SingleSnippetComponent } from './components/single-snippet/single-snippet.component';
 import { SingleArticleComponent } from './components/single-article/single-article.component';
 import { ViewPostComponent } from './components/view-post/view-post.component';
 
 const components = [
   PageNotFoundComponent,
-  SinglePostComponent,
   SingleSnippetComponent,
   SingleArticleComponent,
   ViewPostComponent,
