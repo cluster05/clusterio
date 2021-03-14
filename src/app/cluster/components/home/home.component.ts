932d1cce921f1cb976baef70cb2d6ce67974cb35
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-14 22:15:09 +0530

    home ui updated

--- src/app/cluster/components/home/home.component.ts
+++ src/app/cluster/components/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  skills = ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'Angular', 'React', 'Flutter', 'MongoDB', 'Node', 'Express'];
+  skill = '';
+  studentCount = 100;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    let index = 0;
+    let n = 0;
+    let up = true;
+
+    setInterval(() => {
+      const word = this.skills[index];
+      const length = word.length;
+
+      if (up) {
+        this.skill = word.slice(0, n);
+        n++;
+      }
+      if (n === length + 1) { up = false; }
+      if (!up) {
+        this.skill = word.slice(0, n);
+        n--;
+      }
+      if (n === 0) {
+        up = true;
+        index++;
+      }
+      if (index === this.skills.length) { index = 0; }
+
+    }, 200);
+  }
 }
