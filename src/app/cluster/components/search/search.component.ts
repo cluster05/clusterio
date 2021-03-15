61bf29b30ce1caae81a9a400a087b31a4597b1e3
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-15 17:21:32 +0530

    search UI added

--- src/app/cluster/components/search/search.component.ts
+++ src/app/cluster/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/constant/interface/post.interface';
 
 @Component({
   selector: 'cluster-search',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+
+  searches: Article[] = [];
 
   constructor() { }
 
