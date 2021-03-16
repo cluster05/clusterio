5cad25307c9223f24066654def7449bf32d24e3a
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-16 09:33:22 +0530

    manage search state

--- src/app/cluster/components/search/search.component.ts
+++ src/app/cluster/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Article } from 'src/app/constant/interface/post.interface';
 import { ArticlesService } from 'src/app/shared/services/articles.service';
 import { Subject, Subscription } from 'rxjs';
@@ -9,11 +9,15 @@ import { debounceTime, distinctUntilChanged, mergeMap } from 'rxjs/operators';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
 
   searches: Article[] = [];
   searchTextChanged = new Subject<string>();
   subscription: Subscription = new Subscription();
+  searchState = {
+    isSearching: false,
+    resultFound: 0
+  };
 
   constructor(private articlesService: ArticlesService) { }
 
@@ -26,7 +30,13 @@ export class SearchComponent implements OnInit {
         mergeMap((search: string) => this.articlesService.searchArticlesByKeyWord(search))
       )
       .subscribe(
-        response => this.searches = response,
+        response => {
+          this.searches = response;
+          this.searchState = {
+            isSearching: false,
+            resultFound: this.searches.length
+          }
+        },
         error => alert(error.error.message)
       );
 
@@ -34,8 +44,18 @@ export class SearchComponent implements OnInit {
 
   searchPostByWord(searchWord: string): void {
     if (searchWord.trim().length > 1) {
+      this.searchState = {
+        isSearching: true,
+        resultFound: 0
+      }
       this.searchTextChanged.next(searchWord);
     }
+    if (searchWord.length === 0) {
+      this.searches = [];
+    }
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
