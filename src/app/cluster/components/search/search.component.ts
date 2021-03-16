27f3b061dbe603f71a1deda3d96a97079af6d2e7
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-16 09:15:46 +0530

    optimized search with rxjs

--- src/app/cluster/components/search/search.component.ts
+++ src/app/cluster/components/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/constant/interface/post.interface';
 import { ArticlesService } from 'src/app/shared/services/articles.service';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'cluster-search',
@@ -10,20 +12,31 @@ import { ArticlesService } from 'src/app/shared/services/articles.service';
 export class SearchComponent implements OnInit {
 
   searches: Article[] = [];
+  searchTextChanged = new Subject<string>();
+  subscription: Subscription = new Subscription();
 
   constructor(private articlesService: ArticlesService) { }
 
   ngOnInit(): void {
+
+    this.subscription = this.searchTextChanged
+      .pipe(
+        debounceTime(800),
+        distinctUntilChanged(),
+        mergeMap((search: string) => this.articlesService.searchArticlesByKeyWord(search))
+      )
+      .subscribe(
+        response => this.searches = response,
+        error => alert(error.error.message)
+      );
+
   }
 
   searchPostByWord(searchWord: string): void {
     if (searchWord.trim().length > 1) {
-      this.articlesService.searchArticlesByKeyWord(searchWord)
-        .subscribe(
-          respone => this.searches = respone,
-          error => console.log(error.error.message)
-        );
+      this.searchTextChanged.next(searchWord);
     }
   }
 
 }
+
