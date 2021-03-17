45c9cdf57ed6ce3a38d8bb4f298459c0fa58b905
cluster05 <ajayakumbhar1234@gmail.com>
2021-03-17 12:50:21 +0530

    added API in service

--- src/app/shared/services/course.service.ts
+++ src/app/shared/services/course.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from 'src/app/constant/interface/course.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  readCourse(courseId: string): Observable<Course> {
+    return this.http.get<Course>(environment.backendBaseURL + '/courses/' + courseId);
+  }
+
+  readAllCourse(): Observable<Course[]> {
+    return this.http.get<Course[]>(environment.backendBaseURL + '/courses');
+  }
+
+  createCourse(payload: Course): Observable<Course> {
+    return this.http.post<Course>(environment.backendBaseURL + '/courses', payload);
+  }
+
+  deleteCourse(courseId: string): Observable<void> {
+    return this.http.delete<void>(environment.backendBaseURL + '/courses/' + courseId);
+  }
+
+  uploadCourseImage(image: FormData): Observable<{ imageURL: string }> {
+    return this.http.post<{ imageURL: string }>(environment.backendBaseURL + '/courses/image/upload/', image);
+  }
+
+  readCourseArticle(courseId: string, articleId: string): Observable<Course> {
+    return this.http.get<Course>(environment.backendBaseURL + '/courses/' + courseId + '/article/' + articleId);
+  }
+
+  createCourseArticle(courseId: string, articleId: string): Observable<Course> {
+    return this.http.post<Course>(environment.backendBaseURL + '/courses/' + courseId + '/article/' + articleId, {});
+  }
+
+  deleteCourseArticle(courseId: string, articleId: string): Observable<Course> {
+    return this.http.delete<Course>(environment.backendBaseURL + '/courses/' + courseId + '/article/' + articleId);
+  }
 }
