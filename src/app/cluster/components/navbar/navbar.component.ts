24e4d517ae06609f9532cfea72704951e3655aeb
cluster05 <ajayakumbhar1234@gmail.com>
2021-05-01 13:12:05 +0530

    [cluster] main navbar move from sidebar to navbar

--- src/app/cluster/components/navbar/navbar.component.ts
+++ src/app/cluster/components/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faTag, faBlog, faSearch, faRssSquare, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGripfire } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'cluster-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  userIcon = faUser;
+
+  bottomBarLinks = [
+    { link: 'courses', title: 'courses', icon: faGripfire },
+    { link: 'articles', title: 'articles', icon: faBlog },
+    { link: 'search', title: 'search', icon: faSearch },
+    { link: 'snippets', title: 'snippets', icon: faRssSquare },
+    { link: 'tags', title: 'tags', icon: faTag }
+  ]
+
 
   constructor() { }
 
